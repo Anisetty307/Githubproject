699e5518cfa1e7631c7340f0f976bfa454df0de0
Ravikanth.Anisetty <Ravikanth.Anisetty@INLPF1AX7GT.icepor.com>
2024-10-02 16:48:44 +0530

    1st commit manage

---
+++ src/Githubmanage/GIthubdemoo.java
@@ -0,0 +1,23 @@
+package Githubmanage;
+
+public class GIthubdemoo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		String Car="yes";
+		
+		if(Car=="yes")
+		{
+			System.out.println(" will get the car ");
+		}
+		else 
+		{
+		    System.out.println("sorry");	    
+		}
+				
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ravikanth.Anisetty
+ *
+ */
+module Githubmanage {
+}
